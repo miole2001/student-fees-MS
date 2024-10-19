13778bb24d2912841af59d2c0453a9fbd8010a99
Ralph Miole <blackspade2001@gmail.com>
2024-10-19 20:53:41 +0800

    sql

--- sfms.sql
+++ sfms.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Oct 16, 2024 at 06:35 PM
+-- Generation Time: Oct 19, 2024 at 02:52 PM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.0.28
 
@@ -45,7 +45,29 @@ CREATE TABLE `accounts` (
 
 INSERT INTO `accounts` (`id`, `first_name`, `last_name`, `email`, `password`, `school_id`, `year-level`, `user_type`, `date_registered`) VALUES
 (1, 'admin', 'ko', 'admin@gmail.com', 'admin', '69-6969', '', 'admin', '2024-10-16 22:16:02'),
-(2, 'Jangelyn', 'Perdez', 'jangelyn@gmail.com', '1234', '22-10287', '2rd', 'student', '2024-10-16 23:39:29');
+(2, 'Jangelyn', 'Perdez', 'jangelyn@gmail.com', '1234', '22-10287', '3rd', 'student', '2024-10-16 23:39:29');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `student_bills`
+--
+
+CREATE TABLE `student_bills` (
+  `id` int(11) NOT NULL,
+  `student_id` varchar(100) NOT NULL,
+  `first_name` varchar(50) NOT NULL,
+  `last_name` varchar(50) NOT NULL,
+  `year_level` varchar(30) NOT NULL,
+  `remaining_balance` int(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `student_bills`
+--
+
+INSERT INTO `student_bills` (`id`, `student_id`, `first_name`, `last_name`, `year_level`, `remaining_balance`) VALUES
+(1, '22-10287', 'Jangelyn', 'Perdez', '3rd', 800);
 
 --
 -- Indexes for dumped tables
@@ -58,6 +80,12 @@ ALTER TABLE `accounts`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `student_bills`
+--
+ALTER TABLE `student_bills`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -66,6 +94,12 @@ ALTER TABLE `accounts`
 --
 ALTER TABLE `accounts`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `student_bills`
+--
+ALTER TABLE `student_bills`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
